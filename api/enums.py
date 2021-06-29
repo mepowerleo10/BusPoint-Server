398a178bbbd7ddb779ddd5dc936a6026fe503935
Mussa Mipawa Shomari <mepowerleo10@gmail.com>
2021-06-29 13:06:45 +0300

    Added db.sqlite3 to tracking

--- api/enums.py
+++ api/enums.py
@@ -2,13 +2,11 @@
 from enum import Enum
 
 class RouteColors(Enum):
-    WHITE = "#FFFFFF"
-    BLACK = "#000000"
-    RED = "#E53935"
-    LIGHT_BLUE = "#4FC3F7"
-    BLUE = "#1E88E5"
-    DARK_BLUE = "#01579B"
-    GREEN = "#4CAF50"
+    WHITE = "#EEEEEE"
+    BLACK = "#9E9E9E"
+    RED = "#EF5350"
+    BLUE = "#00BCD4"
+    GREEN = "#9CCC65"
     DEEP_GREEN = "#1B5E20"
     YELLOW = "#FDD835"
 
