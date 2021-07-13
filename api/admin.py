08ce4b0e4134bee09da1f69bf2b9b2892fa682c4
Mussa Mipawa Shomari <mepowerleo10@gmail.com>
2021-07-13 21:17:12 +0300

    Fix: Routing with skippable stops, missing routes due to Decimal zero overlflow

--- api/admin.py
+++ api/admin.py
@@ -24,14 +24,23 @@ class StopInfoForm(ModelForm):
 @admin.register(StopInfo)
 class StopInfoAdmin(admin.ModelAdmin):
     form = StopInfoForm
-    search_fields = ['stop__name', 'route__name']
+    search_fields = ['stop__name', 'route__name', 'weight']
+    list_display = ('stop', 'route', 'weight')
+    list_filter = ['weight', 'route']
 
 @admin.register(Stop)
 class StopAdmin(admin.ModelAdmin):
     search_fields = ['name', 'lat', 'lon']
+    list_display = ('name', 'lat', 'lon')
+
+@admin.register(Journey)
+class JourneyAdmin(admin.ModelAdmin):
+    search_fields = ['routes']
+    list_display = ('__str__', 'start_stop', 'final_stop', 'mid_stop', 'cost')
+    list_filter = ('routes',)
 
 # admin.site.register(Route)
 # admin.site.register(Stop)
 # admin.site.register(StopInfo)
 
-admin.site.register(Journey)
+# admin.site.register(Journey)
--- api/models.py
+++ api/models.py
@@ -6,8 +6,8 @@ from sortedm2m.fields import SortedManyToManyField
 # Create your models here.
 class Stop(models.Model):
     name = models.CharField(max_length=80)
-    lat = models.FloatField(max_length=8)
-    lon = models.FloatField(max_length=8)
+    lat = models.CharField(max_length=11)
+    lon = models.CharField(max_length=11)
 
     def __str__(self):
         return self.name
@@ -15,12 +15,17 @@ class Stop(models.Model):
 
 class Route(models.Model):
     name = models.CharField(max_length=80)
-    start_point = models.ForeignKey(Stop, on_delete=models.CASCADE, related_name="start_point")
-    end_point = models.ForeignKey(Stop, on_delete=models.CASCADE, related_name="end_point")
+    start_point = models.ForeignKey(Stop, on_delete=models.CASCADE, 
+        related_name="start_point")
+    end_point = models.ForeignKey(Stop, on_delete=models.CASCADE, 
+        related_name="end_point")
     forward = models.BooleanField(default=True)
-    bus_stops = models.ManyToManyField(Stop, through='StopInfo' ,related_name="bus_stops")
-    first_stripe = models.CharField(choices=RouteColors.choices(), default=RouteColors.WHITE, max_length=9)
-    last_stripe = models.CharField(choices=RouteColors.choices(), default=RouteColors.WHITE, max_length=9)
+    bus_stops = models.ManyToManyField(Stop, through='StopInfo' ,
+        related_name="bus_stops")
+    first_stripe = models.CharField(choices=RouteColors.choices(), 
+        default=RouteColors.WHITE, max_length=9)
+    last_stripe = models.CharField(choices=RouteColors.choices(),
+        default=RouteColors.WHITE, max_length=9)
 
     def __str__(self):
         return self.name
@@ -29,10 +34,16 @@ class Route(models.Model):
         if other != None:
             return self.__dict__ == other.__dict__ """
 
+class StopWeight(models.IntegerChoices):
+    NORMAL = 1, 'Normal'
+    ROUTABLE = 2, 'Routable'
+
 class StopInfo(models.Model):
     stop = models.ForeignKey(Stop, on_delete=models.CASCADE)
     route = models.ForeignKey(Route, on_delete=models.CASCADE)
     order = models.PositiveIntegerField()
+    weight = models.IntegerField(choices=StopWeight.choices,
+        default=StopWeight.NORMAL)
 
 class Journey(models.Model):
     """ def __init__(self, start_stop, final_stop, mid_stop, notify_stops, routes, cost):
@@ -45,9 +56,12 @@ class Journey(models.Model):
     date = models.DateTimeField(auto_now_add=True, blank=True)
     # location_name = models.CharField(max_length=50)
     # destination_name = models.CharField(max_length=50)
-    start_stop = models.ForeignKey(Stop, on_delete=models.CASCADE, related_name="start_stop")
-    final_stop = models.ForeignKey(Stop, on_delete=models.CASCADE, related_name="final_stop")
-    mid_stop = models.ForeignKey(Stop, null=True, on_delete=models.CASCADE, related_name="mid_stop")
+    start_stop = models.ForeignKey(Stop, on_delete=models.CASCADE, 
+        related_name="start_stop")
+    final_stop = models.ForeignKey(Stop, on_delete=models.CASCADE, 
+        related_name="final_stop")
+    mid_stop = models.ForeignKey(Stop, null=True, on_delete=models.CASCADE, 
+        related_name="mid_stop")
     notify_stops = SortedManyToManyField(Stop, related_name="notify_stops")
     routing_stops = SortedManyToManyField(Stop, related_name="routing_stops")
     routes = SortedManyToManyField(Route, related_name="routes")
